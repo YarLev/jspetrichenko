b11f4cb0c6835dd36b05d94600afcd7b501cf9dc
yalevbrest <info@i-com.by>
2021-07-01 18:44:54 +0300

    add new elements for task

--- js/app.js
+++ js/app.js
@@ -63,3 +63,9 @@ const personalMovieDB = {
       });
    }
 };
+
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button')[1];
+console.log(btns);
